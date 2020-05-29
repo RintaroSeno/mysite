3a80fc713a1f3763d6cff3f62862cc42d9600bb5
Rintaro <BlSp3212@gmail.com>
2020-05-29 10:20:32 +0900

    first product

--- Python-Lesson/script.py
+++ Python-Lesson/script.py
@@ -1,2 +1,12 @@
-print('Hello, World!')
-print(1 + 2)
+# coding:utf-8
+answers = ['はい', 'いいえ']
+index = 0
+
+for answer in answers:
+    print(str(index) + '.' + answer)
+    index += 1
+input_answer = int(input('勉強は好きですか？番号で答えてください：'))
+if input_answer == 0:
+    print('素晴らしいですね！')
+else :
+    print('私もです')
